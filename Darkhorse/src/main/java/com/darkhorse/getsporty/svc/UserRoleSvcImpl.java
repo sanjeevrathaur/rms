7d432b01ff1988dbd0c1ddcd8be65673eedacd50
Sanjeev Rathaur <this.sanjeev@gmail.com>
2017-03-08 14:55:50 +0530

    code comments

--- Darkhorse/src/main/java/com/darkhorse/getsporty/svc/UserRoleSvcImpl.java
+++ Darkhorse/src/main/java/com/darkhorse/getsporty/svc/UserRoleSvcImpl.java
@@ -5,6 +5,7 @@ import com.darkhorse.getsporty.domain.UserRole;
 /** test comments */
 public class UserRoleSvcImpl implements UserRoleSvc {
 	
+	/** find user role by role code*/
 	public UserRole findByCode(String code){
 		return userRoleDao.findByCode(code);
 		
